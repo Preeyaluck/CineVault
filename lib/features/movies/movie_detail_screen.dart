b654c48e261b75a9b676e3cffa9357ca518311ee
Preeyaluck <6731503021@lamduan.mfu.ac.th>
2026-02-19 23:25:43 +0700

    first commit

--- lib/features/movies/movie_detail_screen.dart
+++ lib/features/movies/movie_detail_screen.dart
@@ -9,14 +9,16 @@ class MovieDetailScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final movie = ref.watch(selectedMovieProvider);
 
-    if (movie == null) return const Scaffold(body: Center(child: Text('No Movie Selected')));
+    if (movie == null)
+      return const Scaffold(body: Center(child: Text('No Movie Selected')));
 
     return Scaffold(
       appBar: AppBar(title: Text(movie.title)),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(movie.posterUrl, width: double.infinity, fit: BoxFit.cover),
+            Image.network(movie.posterUrl,
+                width: double.infinity, fit: BoxFit.cover),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(movie.summary, style: const TextStyle(fontSize: 16)),
